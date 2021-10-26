8e0bb84e19cdc4517bbc2046aa89f4b653e43b76
Matthew Torres <matthew.torres211@gmail.com>
2021-10-27 00:56:35 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app.css'
+
+export const App = () => {
+  return (
+    <div className='app'>
+      <div className="container">
+        React
+      </div>
+    </div>
+  )
+}
