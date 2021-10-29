1e6e0502411f0ed8f327d99fc43706a4269fbe0b
Matthew Torres <matthew.torres211@gmail.com>
2021-10-30 01:37:09 +0800

    Add message

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import './app.css'
+import React from "react";
+import "./app.css";
 
 export const App = () => {
   return (
-    <div className='app'>
+    <div className="app">
       <div className="container">
-        React
+        <p className="quote">
+          "I love being married. It's so great to find that one special person
+          you want to annoy for the rest of your life." <br />– Rita Rudner
+        </p>
+        <p>
+          While searching for the best quote that fits perfectly with the
+          relationship that you guys have, I happened to read this quote and I
+          know it is perfect. Unlike this quote love and marriage will never be
+          perfect, far from it. It is a journey of two people working things
+          out, discovering each other, understanding each other's shortcomings,
+          fighting over small things, and most importantly forgiving each other.
+          In the three beautiful, loud, and awesome months that I've been with
+          you guys, I've come to realize what made your relationship lasts when
+          others cannot - it's <strong>the fights</strong>. Well, it's what
+          comes after those fights - <strong>forgiveness.</strong>
+        </p>
+        <p>
+          According to Wikipedia, forgiveness is the intentional and voluntary
+          process by which one who may initially feel victimized, undergoes a
+          change in feelings and attitude regarding a given offense, and{" "}
+          <strong>
+            overcomes negative emotions such as resentment and vengeance
+          </strong>
+          . I noticed that your fights rarely extend to the next day. You guys
+          try to work things out, although sometimes while shouting and banging
+          doors, but what's important is that each of you forgives.
+          Nevertheless, I hope that you guys will lessen your fights now that
+          you are technically a family. Always remember that it's not you versus
+          your partner, rather you versus the problem that you are tackling.
+        </p>
+        <p>
+          I would also like to take this opportunity to thank you for "adopting"
+          me into your lives. It is rare to find true friends when you grow old,
+          and I am lucky to have found two. I am forever grateful for the help
+          and encouragement that you guys have given me during my turbulent
+          times. You guys were my rock when I felt overwhelmed with the problems
+          I was facing. You guys have given me a new perspective in life and
+          helped me reflect and rethink what is important -{" "}
+          <strong>life</strong>.{" "}
+        </p>
+        <p>
+          <div className="salutations">To Lu Dan,</div>
+          Life, especially creating a new one, is a wonderful experience (or so
+          they said, don't really have any kids myself). I hope that you won't
+          be afraid in facing this new chapter in your life. I know that you are
+          afraid and concerned about raising kids and all its nuisance but
+          knowing you and with Albert's help you will be able to overcome it. I
+          also hope that you will lengthen your patience with Albert (cause you
+          know, it's Albert!!!!), lessen the volume of your voice, and try not
+          to throw your frustrations at Albert when you are stressed.{" "}
+        </p>
+        <p>
+          <div className="salutations">To Albert,</div>
+          Stress, sometimes you are the reason why Lu Dan is stressed.
+          Hopefully, you can be less annoying to Lu Dan when she is stressed
+          (but don't remove it. It won't be Albert anymore if you're not
+          annoying). Continue with what you're doing now, but I think it won't
+          hurt when you give Lu Dan some surprises, I know girls love surprises
+          (wink wink).{" "}
+        </p>
+        <p>
+          Best wishes and congratulations on this new chapter of your lives. I
+          hope when I leave, it won't be the last time I'll be seeing you. I'll
+          miss you guys. Love you. <br /> <br />
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
